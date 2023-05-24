40a03684456da5a6c36c27d1ef44e4bbbdda5894
Allan <acquiredge@gmail.com>
2023-05-24 09:37:51 -0700

    adding spread operator exercises to variableArguments.js

--- Class7_JS_Functions/exerciseTemplates/VariableArguments.js
+++ Class7_JS_Functions/exerciseTemplates/VariableArguments.js
@@ -33,3 +33,30 @@ function mergeStrings() {
 // Sample invocations:
 // console.log(mergeStrings('Hello', 'World')); // Expected output: 'HelloWorld'
 // console.log(mergeStrings('This', 'is', 'a', 'test')); // Expected output: 'Thisisatest'
+
+// Exercise 5: Write a function that combines two arrays of numbers.
+function addNumbers(array1, array2) {
+    // your code here
+}
+
+// Sample invocations:
+// console.log(addNumbers([1,2,3], [4,5,6])); // expected output: [1,2,3,4,5,6]
+// console.log(addNumbers([7,8,9], [10,11,12])); // expected output: [7,8,9,10,11,12]
+
+// Exercise 6: The function should take two objects as arguments and return a single object that combines all of the properties of the input objects. 
+// If the same key exists in both input objects, the value from the second input object should be used. 
+function mergeObjects(object1, object2) {
+    // your code here
+}
+
+// Sample invocations:
+// console.log(mergeObjects({a: 1}, {b: 2})); // expected output: {a: 1, b: 2}
+// console.log(mergeObjects({a: 1}, {a: 2})); // expected output: {a: 2}
+
+// Exercise 7: The function should take a string as an argument and return an array of the individual characters in the string.
+function spreadString(string) {
+    // your code here
+}
+// Sample invocations:
+// console.log(spreadString('hello')); // expected output: ['h', 'e', 'l', 'l', 'o']
+// console.log(spreadString('world')); // expected output: ['w', 'o', 'r', 'l', 'd']
